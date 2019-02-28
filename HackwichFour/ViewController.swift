53c1afcdbe944f01c86001b3e42d69986c935752
Marion Ano <wahinecoder@gmail.com>
2019-02-28 09:39:41 -1000

    Initial Commit

---
+++ HackwichFour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackwichFour
+//
+//  Created by Marion Ano on 2/28/19.
+//  Copyright © 2019 Marion Ano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
