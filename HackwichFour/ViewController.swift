f342f89067286293bdc818a2bbf0fa10e1aa4b1c
Marion Ano <wahinecoder@gmail.com>
2019-03-06 21:57:24 -1000

    Completed Hackwich#4

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -8,7 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//declare our array
+
+  
+    var myFriends = ["Ryan", "Nicole", "Sara", "Tyler"]
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return myFriends.count
+        //return 4
+
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Code Block
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = myFriends[indexPath.row]
+        cell.textLabel?.text=text
+        return cell
+    }
+
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
